52e72734ae1285b723ef423ff351ab00091ad68d
sgwalherkar <sgwalherkar@systango.com>
2022-11-24 15:02:47 +0530

    Initial commit

---
+++ Example/DemoLibrary/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  DemoLibrary
+//
+//  Created by gwalherkarswapnil on 11/24/2022.
+//  Copyright (c) 2022 gwalherkarswapnil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
